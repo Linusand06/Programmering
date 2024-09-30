d383f72c47daee6ce9ebd81f25dfec521a68ccd8
Linusand06 <linus.andersson0611@outlook.com>
2024-09-30 14:12:55 +0200

    kjbhwarg

--- Programmering 1/Python Projekt/Slagsmålsspel.py
+++ Programmering 1/Python Projekt/Slagsmålsspel.py
@@ -9,18 +9,16 @@ playerb = 100
 while (playera >0 and playerb >0 ):
     damage = random.randint(5,15)
     playera -= damage
-    print (damage)
     print("Player A hits player B with the force of ", damage, "damage points")
     if playera <= 0:
-        print( "Player B has won!")
+        print( namnb, "has won!")
         break
 
     damage = random.randint(5,15)
     playerb -= damage
-    print (damage)
     print("Player B hits player A with the force of ", damage, "damage points")
     if playerb <= 0:
-        print("Player A has won!")
+        print(namna, "has won!")
         break
 
 
