fa12eaee9b990712014ebbadf0f45dda147e3828
Linusand06 <linus.andersson0611@outlook.com>
2024-10-07 14:07:21 +0200

    v2

--- Programmering 1/Python Projekt/Slagsmålsspel.py
+++ Programmering 1/Python Projekt/Slagsmålsspel.py
@@ -1,26 +1,36 @@
 import random
+import time
 
-namna=input("Skriv namn på spelare A: ")
-namnb=input("Skriv namn på spelare B: ")
+while True:  
 
-playera = 100
-playerb = 100
+    namna = input("Choose Player A name: ")
+    namnb = input("Choose Player B name: ")
 
-while (playera >0 and playerb >0 ):
-    damage = random.randint(5,15)
-    playera -= damage
-    print("Player A hits player B with the force of ", damage, "damage points")
-    if playera <= 0:
-        print( namnb, "has won!")
+    playera = 100
+    playerb = 100
+
+    while playera > 0 and playerb > 0:
+        damage = random.randint(5, 15)
+        playerb -= damage
+        print("Player A hits player B with the force of", damage, "damage points")
+        if playerb <= 0:
+            print(namna, "has won!")
+            break
+
+        damage = random.randint(5, 15)
+        playera -= damage
+        print("Player B hits player A with the force of", damage, "damage points")
+        if playera <= 0:
+            print(namnb, "has won!")
+            break
+
+        print(namna, "has", playera, "HP left")
+        print(namnb, "has", playerb, "HP left")
+
+
+        time.sleep(1)
+
+    restart = input("Do you want to play again? (yes/no): ").lower()
+    if restart != "yes":
+        print("Thanks for playing!")
         break
-
-    damage = random.randint(5,15)
-    playerb -= damage
-    print("Player B hits player A with the force of ", damage, "damage points")
-    if playerb <= 0:
-        print(namna, "has won!")
-        break
-
-
-else: 
-    print("Tied!" )
